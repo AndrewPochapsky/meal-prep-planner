985e784473d7bcd3de711f40efd755b2edf8f350
AndrewPochapsky <andrew.pochapsky@gmail.com>
2019-12-21 10:01:50 -0500

    added basic components

---
+++ mealplanner/frontend/src/components/meals/Meals.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class Meals extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Meals List </h1>
+      </div>
+    );
+  }
+}
+export default Meals;
