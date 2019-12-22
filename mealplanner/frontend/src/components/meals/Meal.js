75e6068c39ed2a9de41f8b492cec1f6a20eab731
AndrewPochapsky <andrew.pochapsky@gmail.com>
2019-12-22 09:43:21 -0500

    repalaced table with cards

---
+++ mealplanner/frontend/src/components/meals/Meal.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { deleteMeal } from "../../actions/meals";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+export class Meal extends Component {
+  static propTypes = {
+    meal: PropTypes.object.isRequired,
+    deleteMeal: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { id, name, description, preparation_time, steps } = this.props.meal;
+    return (
+      <div className="card" style={{ width: "25%" }}>
+        <div className="card-body d-flex flex-column">
+          <h5 className="card-title">{name}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{preparation_time}</h6>
+          <p className="card-text">{description}</p>
+          <div className="btn-toolbar mt-auto">
+            <div className="button-group">
+              <button className="btn btn-primary btn-sm">Edit</button>
+            </div>
+            <div className="button-group">
+              <button
+                onClick={this.props.deleteMeal.bind(this, id)}
+                className="btn btn-danger btn-sm"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default connect(null, { deleteMeal })(Meal);
--- mealplanner/frontend/src/components/meals/Meals.js
+++ mealplanner/frontend/src/components/meals/Meals.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getMeals, deleteMeal } from "../../actions/meals";
+import Meal from "./Meal";
 
 export class Meals extends Component {
   static propTypes = {
@@ -18,35 +19,11 @@ export class Meals extends Component {
     return (
       <Fragment>
         <h2>Meals</h2>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Preparation Time</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.meals.map(meal => (
-              <tr key={meal.id}>
-                <td>{meal.id}</td>
-                <td>{meal.name}</td>
-                <td>{meal.description}</td>
-                <td>{meal.preparation_time}</td>
-                <td>
-                  <button
-                    onClick={this.props.deleteMeal.bind(this, meal.id)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div className="row container">
+          {this.props.meals.map(meal => (
+            <Meal key={meal.id} meal={meal} />
+          ))}
+        </div>
       </Fragment>
     );
   }
