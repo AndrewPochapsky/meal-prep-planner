8b6a698a14290b48478c6c4dbff0a0d5db9fff4a
AndrewPochapsky <andrew.pochapsky@gmail.com>
2019-12-25 16:39:26 -0500

    can now view meal info in a non-edit mode

--- mealplanner/frontend/src/components/meals/ViewMeal.js
+++ mealplanner/frontend/src/components/meals/ViewMeal.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Step from "./Step";
 import PropTypes from "prop-types";
 
 export class ViewMeal extends Component {
@@ -14,9 +15,24 @@ export class ViewMeal extends Component {
       steps
     } = this.props.location.meal;
     return (
-      <div>
-        <h1> {name} </h1>
-      </div>
+      <Fragment>
+        <Fragment>
+          <div className="card text-center" style={{ witdth: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">{name}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                {preparation_time} minutes
+              </h6>
+              <p className="card-text">{description}</p>
+            </div>
+          </div>
+        </Fragment>
+        <Fragment>
+          {steps.map(step => (
+            <Step key={step.id} step={step} isEditMeal={false} />
+          ))}
+        </Fragment>
+      </Fragment>
     );
   }
 }
